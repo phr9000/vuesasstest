62de540f3cc4fb87d51a6bf7748e1ef49da3beee
phr9000 <phr9000@gmail.com>
2022-05-09 23:53:06 +0900

    first

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="main-container">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="container">
+      <div class="child1">test1</div>
+      <div class="child2">test2</div>
+      <div class="child3 none">test3</div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style lang="scss" scoped>
+@import "@/assets/scss/abstract/_functions.scss";
+@import "@/assets/scss/abstract/_mixins.scss";
+@import "@/assets/scss/abstract/_variables.scss";
+.main-container {
+  @include set-flex(column, center, center);
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding-top: 6rem;
+  .container {
+    .child1 {
+      @include set-font($font-base, 700, $red2);
+    }
+    .child3 {
+      &.none {
+        display: none;
+      }
+    }
+  }
 }
 </style>
